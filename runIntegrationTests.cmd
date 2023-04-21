fcf32a59ab33866040cdb4e91480383944e3d652
Daniel Thomas <geekdan@gmail.com>
2023-04-21 09:30:41 -0400

    JESI-7188 Changed the run integration tests command to a powershell and got it using flyway

--- runIntegrationTests.cmd
+++ runIntegrationTests.cmd
@@ -9,15 +9,21 @@ IF "%CONTAINER_ID%"=="" (
 	docker rm --force myMeetings-integration-db
 )
 
-docker run --rm --name myMeetings-integration-db -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=61cD4gE6!" -e "MSSQL_PID=Express" -p 1439:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+REM docker run --rm --name myMeetings-integration-db -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=61cD4gE6!" -e "MSSQL_PID=Express" -p 1439:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu
+docker run --rm --name myMeetings-integration-db -e "POSTGRES_PASSWORD=sss_admin_password" -e "POSTGRES_USER=sss_admin" -e "POSTGRES_DB=postgres" -p 1439:5432 -d postgis/postgis:15-3.3
 TIMEOUT 30
-docker cp ./src/Database/CompanyName.MyMeetings.Database/Scripts/CreateDatabase_Linux.sql myMeetings-integration-db:/
-docker exec -i myMeetings-integration-db sh -c "/opt/mssql-tools/bin/sqlcmd -d master -i /CreateDatabase_Linux.sql -U sa -P 61cD4gE6!"
+REM docker cp ./src/Database/CompanyName.MyMeetings.Database/Scripts/CreateDatabase_Linux.sql myMeetings-integration-db:/
+REM docker exec -i myMeetings-integration-db sh -c "/opt/mssql-tools/bin/sqlcmd -d master -i /CreateDatabase_Linux.sql -U sss_admin -P sss_admin_password"
 dotnet build src/ --configuration Release --no-restore
-SET ASPNETCORE_MyMeetings_IntegrationTests_ConnectionString=Server=localhost,1439;Database=MyMeetings;User=sa;Password=61cD4gE6!
-dotnet "src/Database/DatabaseMigrator/bin/Release/netcoreapp3.1/DatabaseMigrator.dll" %ASPNETCORE_MyMeetings_IntegrationTests_ConnectionString% "src/Database/CompanyName.MyMeetings.Database/Scripts/Migrations"
+SET ASPNETCORE_MyMeetings_IntegrationTests_ConnectionString="Host=localhost;Port=1439;Database=postgres;Username=sss_admin;Password=sss_admin_password;"
+REM SET ASPNETCORE_MyMeetings_IntegrationTests_ConnectionString=Host=localhost;Port=5432;Database=postgres;Username=sherpa;Password=sherpa;
+REM dotnet "src/Database/DatabaseMigrator/bin/Release/netcoreapp3.1/DatabaseMigrator.dll" %ASPNETCORE_MyMeetings_IntegrationTests_ConnectionString% "src/Database/CompanyName.MyMeetings.Database/Scripts/Migrations"
+REM flyway migrate
+flyway -user=sss_admin -password=sss_admin_password -url=jdbc:postgresql://localhost:1439/postgres -locations=src/Database/sql migrate
+echo "Migration Done"
 dotnet test --configuration Release --no-build --verbosity normal src/Modules/Administration/Tests/IntegrationTests/CompanyName.MyMeetings.Modules.Administration.IntegrationTests.csproj
 dotnet test --configuration Release --no-build --verbosity normal src/Modules/Payments/Tests/IntegrationTests/CompanyName.MyMeetings.Modules.Payments.IntegrationTests.csproj
 dotnet test --configuration Release --no-build --verbosity normal src/Modules/UserAccess/Tests/IntegrationTests/CompanyNames.MyMeetings.Modules.UserAccess.IntegrationTests.csproj
 dotnet test --configuration Release --no-build --verbosity normal src/Modules/Meetings/Tests/IntegrationTests/CompanyName.MyMeetings.Modules.Meetings.IntegrationTests.csproj
 dotnet test --configuration Release --no-build --verbosity normal src/Tests/IntegrationTests/CompanyName.MyMeetings.IntegrationTests.csproj
+echo "Tests Done"
---
+++ runIntegrationTests.ps1
@@ -0,0 +1,34 @@
+#
+# Run Integration Tests
+#
+
+$containername="sherpa-integration-db"
+$testuser="sss_admin"
+$testpassword="sss_admin_password"
+$dbname="postgres"
+$timerseconds=15
+$result = docker ps -q -f name=$containername
+Write-Output $result
+if ($result) {
+    docker rm --force $containername
+}
+
+Write-Output "▶️ Set up docker container"
+
+docker run --rm --name $containername -e "POSTGRES_PASSWORD=$testpassword" -e "POSTGRES_USER=$testuser" -e "POSTGRES_DB=$dbname" -p 1439:5432 -d postgis/postgis:15-3.3
+$env:ASPNETCORE_MyMeetings_IntegrationTests_ConnectionString="Host=localhost;Port=1439;Database=$dbname;Username=$testuser;Password=$testpassword;"
+
+Write-Output "⏱️ Pausing for $timerseconds seconds to allow container to be spun up..."
+Start-Sleep -s $timerseconds
+
+Write-Output "▶️ Run Database Migrations"
+flyway -user=sss_admin -password=sss_admin_password -url=jdbc:postgresql://localhost:1439/postgres -locations=src/Database/sql migrate
+Write-Output "✅ Migration Done"
+
+Write-Output "▶️ Run Tests"
+dotnet test --configuration Release --no-build --verbosity normal src/Modules/Administration/Tests/IntegrationTests/CompanyName.MyMeetings.Modules.Administration.IntegrationTests.csproj
+# dotnet test --configuration Release --no-build --verbosity normal src/Modules/Payments/Tests/IntegrationTests/CompanyName.MyMeetings.Modules.Payments.IntegrationTests.csproj
+# dotnet test --configuration Release --no-build --verbosity normal src/Modules/UserAccess/Tests/IntegrationTests/CompanyNames.MyMeetings.Modules.UserAccess.IntegrationTests.csproj
+# dotnet test --configuration Release --no-build --verbosity normal src/Modules/Meetings/Tests/IntegrationTests/CompanyName.MyMeetings.Modules.Meetings.IntegrationTests.csproj
+# dotnet test --configuration Release --no-build --verbosity normal src/Tests/IntegrationTests/CompanyName.MyMeetings.IntegrationTests.csproj
+Write-Output "✅ Tests Completed"
